fb47d8d83a0f239d8d0b278fa2eda6cf9e15eeff
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-09-25 16:02:38 -0400

    Create math-utils.h

---
+++ src/math-utils.h
@@ -0,0 +1,17 @@
+//
+// Created by loona on 25/09/22.
+//
+
+#pragma once
+
+#include "math.h"
+
+#include "angles.h"
+
+// lin alg
+#include "lin_alg/lines.h"
+#include "lin_alg/matrix.h"
+#include "lin_alg/planes.h"
+#include "lin_alg/vector_utils.h"
+
+using pi = M_PI;
